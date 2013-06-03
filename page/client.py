7c50269f7dd924240a404940c2c0762b35ca0272
Mike Cooper <mythmon@gmail.com>
2013-06-02 20:27:43 -0700

    Basic highlight watching

--- page/client.py
+++ page/client.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol, ClientFactory
@@ -18,8 +19,9 @@ class RelayProtocol(Protocol):
     def connectionMade(self):
         self.transport.write('init password={password},compression=off\n'
                              .format(**config))
-        self.transport.write('(buffer_list) hdata buffer:gui_buffers(*) '
-                             'number,full_name\n')
+        # self.transport.write('(buffer_list) hdata buffer:gui_buffers(*) '
+        #                      'number,full_name\n')
+        self.transport.write('sync\n')
 
     def dataReceived(self, data):
         self._buffer += data
@@ -30,18 +32,34 @@ class RelayProtocol(Protocol):
             to_parse = self._buffer[:length]
             self._buffer = self._buffer[length:]
 
-            print 'got data (%s bytes)' % length
             msg_id, message = parse_message(to_parse)
-            print 'id:', msg_id
 
-            if msg_id == 'buffer_list':
-                for v in message[0]['values']:
-                    print v['number'], v['full_name']
-                self.end()
+            if msg_id == '_buffer_line_added':
+                for msg in message:
+                    for line in msg['values']:
+                        self.process_line(line)
 
     def end(self):
         self.transport.write('quit\n')
         self.transport.loseConnection()
+
+    def process_line(self, line):
+        if line['highlight'] == '\x00':
+            return
+        if line['displayed'] == '\x00':
+            return
+        if 'irc_privmsg' not in line['tags_array']:
+            return
+
+        print self.clean_colors('{prefix}: {message}'.format(**line))
+
+    def clean_colors(self, message):
+        clean = re.sub(r'\x19F\d\d', '', message)
+
+        if '\x19' in clean:
+            raise NotImplemented('Unknown format character in "%r".' % clean)
+
+        return clean
 
 
 class RelayFactory(ClientFactory):
